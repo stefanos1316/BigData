85a14c499875ba4abfb673a35284842c9c380081
stefanos1316 <stefanos1316@gmail.com>
2018-03-01 11:33:39 +0200

    More fixes

--- tools/travis-deploy.sh
+++ tools/travis-deploy.sh
@@ -67,7 +67,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $encrypted_24f3b62cf69b_key -iv $encrypted_24f3b62cf69b_iv -in deploy_key.enc -out ~\/.ssh/deploy_key -d
+openssl aes-256-cbc -K $encrypted_24f3b62cf69b_key -iv $encrypted_24f3b62cf69b_iv -in ../deploy_key.enc -out ../deploy_key -d
 
 if [ -n "$DEBUG" ] ; then
   set -x
